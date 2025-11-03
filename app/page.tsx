f67db8fe0813805b746e05e3b0a318cb11668571
Bishal Thapaliya <bishalthapaliya@Bishals-MacBook-Pro.local>
2025-11-03 13:00:45 +0100

    add why choose us section with sticky sroll effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/app/components/Hero";
 import FeaturedCourses from "@/app/components/FeaturedCourses";
+import WhyChooseUs from "@/app/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <Hero />
       <FeaturedCourses />
+      <WhyChooseUs />
     </main>
   );
 }
