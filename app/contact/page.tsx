4cf40fb4a43622ae25efd73e9b8355fc8d1abb4b
Bishal Thapaliya <bishalthapaliya@Bishals-MacBook-Pro.local>
2025-11-04 17:14:08 +0100

    fix typo in className

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -16,7 +16,7 @@ const ContactUs = () => {
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900 py-12 py-12 pt-36 relative">
       <BackgroundBeams className="absolute top-0 left-0 w-full h-full z-0" />
 
-      <div className="max-w-2xl mx-auto p-4 reltaive z-10">
+      <div className="max-w-2xl mx-auto p-4 relative z-10 ">
         <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">Contact Us</h1>
         <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center">
           We&apos;re here to help with any questions about our courses,
